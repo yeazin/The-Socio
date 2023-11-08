b5d860d6926b90461df189db88c547a6cd6e133e
yeazin <naz.yeasin@gmail.com>
2023-11-08 18:47:03 +0600

    Accounts app Initialized

---
+++ socio/accounts/models.py
@@ -0,0 +1,37 @@
+## Account & timestamp system 
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from accounts.managers import CustomUserManager
+from util_base.models._initModels import TimeStampMixin
+
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=200, blank=True, unique=True, null=True)
+    email = models.EmailField("email_address", unique=True, null=True, blank=True)
+
+    password = models.CharField(max_length=1500, null=True)
+    confirm_password = models.CharField(max_length=1500, null=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    objects = CustomUserManager()
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.username)
+
+    class Meta:
+        verbose_name_plural = "User"
+        indexes = [models.Index(fields=["id", "username"])]
+
+
+
+
